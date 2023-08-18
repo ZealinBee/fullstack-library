8887dee05e37d007392a9c7ac4fedebb753ec3cd
Zhiyuan Liu <thenukezealot@gmail.com>
2023-08-18 16:06:12 +0300

    readme.md

--- trial/IntegrifyLibrary.Domain/src/Entities/Author.cs
+++
@@ -1,9 +0,0 @@
-namespace IntegrifyLibrary.Domain
-{
-    public record Author
-    {
-        public Guid Id { get; init; }
-        public Guid BookId { get; init; }
-        public string AuthorName { get; init; }
-    }
-}
--- trial/IntegrifyLibrary.Domain/src/Entities/LoanDetails.cs
+++
@@ -1,8 +0,0 @@
-namespace IntegrifyLibrary.Domain
-{
-    public record LoanDetails
-    {
-        public Guid LoanId { get; init; }
-        public Guid BookId { get; init; }
-    }
-}
