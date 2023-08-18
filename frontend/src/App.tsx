106ed580f64dff95110e661b6f662fa83811a038
Zhiyuan Liu <thenukezealot@gmail.com>
2023-08-18 18:51:17 +0300

    initial set up for the routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AuthPage from './pages/AuthPage';
+import HomePage from "./pages/HomePage";
+import NotFoundPage from './pages/NotFoundPage';
+import CartPage from './pages/CartPage';
+import DashboardPage from './pages/DashboardPage';
+import LandingPage from './pages/LandingPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/auth",
+    element: <AuthPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/welcome",
+    element: <LandingPage />,
+    errorElement: <NotFoundPage />,
+  }
+])
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
